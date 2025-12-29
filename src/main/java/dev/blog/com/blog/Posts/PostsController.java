c89d96fa30e9434772f75f46e251b6dfd07990e8
Mateus Santana <mateusantana@proton.me>
2025-12-29 15:40:58 -0300

    feat: Atualizando o controller com o mock de dados

--- src/main/java/dev/blog/com/blog/Posts/PostsController.java
+++ src/main/java/dev/blog/com/blog/Posts/PostsController.java
@@ -1,4 +1,32 @@
 package dev.blog.com.blog.Posts;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/posts")
 public class PostsController {
+    @PostMapping
+    public String create() {
+        return "Post created successfully";
+    }
+    @GetMapping
+    public String findAll() {
+        return "List of all Post";
+    }
+
+    @GetMapping("/{id}")
+    public String findById(@PathVariable Long id) {
+        return "Post with id: " + id;
+    }
+
+    @PutMapping("/{id}")
+    public String update(@PathVariable Long id) {
+        return "Post with ID " + id + " updated";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable Long id) {
+        return "Post with ID " + id + " deleted";
+    }
+
 }
