546e503c982474b6b6c750593d2142240702030e
Mateus Santana <mateusantana@proton.me>
2025-12-28 22:08:16 -0300

    feat: Adicionando o Lombok nos models de Admin e Posts

--- src/main/java/dev/blog/com/blog/Admins/AdminModel.java
+++ src/main/java/dev/blog/com/blog/Admins/AdminModel.java
@@ -1,11 +1,18 @@
 package dev.blog.com.blog.Admins;
 import dev.blog.com.blog.Posts.PostsModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
 // Diz que isso é uma entidade para o banco de dados;
 @Entity
 @Table(name = "tb_admin")
+@AllArgsConstructor// Lombok
+@NoArgsConstructor // Lombok
+@Data // Lombok - cria os Getters e os Setters
 public class AdminModel {
 
     @Id
@@ -19,55 +26,4 @@ public class AdminModel {
 
     @OneToMany(mappedBy = "admin")
     private List<PostsModel> posts;
-
-    public AdminModel() {
-    }
-
-    public AdminModel(String name, String email, String password, String profilePicture, String bio) {
-        this.name = name;
-        this.email = email;
-        this.password = password;
-        this.profilePicture = profilePicture;
-        this.bio = bio;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getProfilePicture() {
-        return profilePicture;
-    }
-
-    public void setProfilePicture(String profilePicture) {
-        this.profilePicture = profilePicture;
-    }
-
-    public String getBio() {
-        return bio;
-    }
-
-    public void setBio(String bio) {
-        this.bio = bio;
-    }
 }
--- src/main/java/dev/blog/com/blog/Posts/PostsModel.java
+++ src/main/java/dev/blog/com/blog/Posts/PostsModel.java
@@ -1,12 +1,17 @@
 package dev.blog.com.blog.Posts;
 import dev.blog.com.blog.Admins.AdminModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
-
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "tb_posts")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class PostsModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,77 +27,9 @@ public class PostsModel {
     private String author;
     private Boolean status;
 
-//    Muitos posts um adm;
+//    Muitos posts para um adm;
     @ManyToOne
     @JoinColumn(name = "admin_id") // Boa prática: define o nome da coluna da FK
     private AdminModel admin;
 
-
-    public PostsModel() {}
-
-    public PostsModel(String title, String content, String imageUrl, LocalDateTime dataPublication, String summary, String author, Boolean status) {
-        this.title = title;
-        this.content = content;
-        this.imageUrl = imageUrl;
-        this.dataPublication = dataPublication;
-        this.summary = summary;
-        this.author = author;
-        this.status = status;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
-
-    public LocalDateTime getDataPublication() {
-        return dataPublication;
-    }
-
-    public void setDataPublication(LocalDateTime dataPublication) {
-        this.dataPublication = dataPublication;
-    }
-
-    public String getSummary() {
-        return summary;
-    }
-
-    public void setSummary(String summary) {
-        this.summary = summary;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public Boolean getStatus() {
-        return status;
-    }
-
-    public void setStatus(Boolean status) {
-        this.status = status;
-    }
 }
