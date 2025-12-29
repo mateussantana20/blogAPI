e8a14d475e79b7130b7eb35254b5f8331123543e
Mateus Santana <mateusantana@proton.me>
2025-12-29 15:09:24 -0300

    feat: Migrations para db com flyway

---
+++ src/main/resources/db/migration/V1__create_tables_admin_and_posts.sql
@@ -0,0 +1,24 @@
+-- Criação da tabela de Administradores
+CREATE TABLE tb_admin (
+                          id BIGSERIAL PRIMARY KEY,
+                          name VARCHAR(255) NOT NULL,
+                          email VARCHAR(255) UNIQUE NOT NULL,
+                          password VARCHAR(255) NOT NULL,
+                          profile_picture VARCHAR(255),
+                          bio TEXT
+);
+
+-- Criação da tabela de Posts
+CREATE TABLE tb_posts (
+                          id BIGSERIAL PRIMARY KEY,
+                          title VARCHAR(255) NOT NULL,
+                          content TEXT NOT NULL,
+                          image_url VARCHAR(255),
+                          data_publication TIMESTAMP NOT NULL,
+                          summary VARCHAR(255),
+                          author VARCHAR(255),
+                          status BOOLEAN,
+                          admin_id BIGINT,
+    -- Chave Estrangeira ligando Post ao Admin
+                          CONSTRAINT fk_post_admin FOREIGN KEY (admin_id) REFERENCES tb_admin(id)
+);
