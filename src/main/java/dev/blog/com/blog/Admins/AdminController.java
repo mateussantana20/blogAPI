b27089fc82412d42e1722251d5da0a487b2a4086
Mateus Santana <mateusantana@proton.me>
2025-12-29 16:30:43 -0300

    fix: Alterando o nome das rotas do Controller e criando o service admin

--- src/main/java/dev/blog/com/blog/Admins/AdminController.java
+++ src/main/java/dev/blog/com/blog/Admins/AdminController.java
@@ -1,17 +1,48 @@
 package dev.blog.com.blog.Admins;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 // Essa anotation diz que essa classe é um controller
 @RestController
 //Mapeia as rotas
-@RequestMapping
+@RequestMapping("/admins")
 public class AdminController {
+
+    private AdminRepository repository; // Injetando a ferramenta que fala com o banco
 
     //GET
     @GetMapping("/boasvinda")
     public String boasVindas() {
-        return "boasVindas";
+        return "Bem-vindo ao sistema de Administração!";
+    }
+
+    // Adicionar admin
+    @PostMapping
+    public AdminModel create(@RequestBody AdminModel admin) {
+//        return "Admin created successfully";
+        return repository.save(admin);
+    }
+
+    // Mostrar todos os admins
+    @GetMapping
+    public String findAll() {
+        return "List of all admins";
+    }
+
+    // Procurar Admin por ID
+    @GetMapping("/{id}")
+    public String findById(@PathVariable Long id) {
+        return "listarAdminID";
+    }
+
+    // Alterar dados do admin
+    @PutMapping("/{id}")
+    public String update(@PathVariable Long id) {
+        return "Admin with ID " + id + " updated";
+    }
+
+    // Deletar admin
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable Long id) {
+        return "Admin with ID " + id + " deleted";
     }
 }
---
+++ src/main/java/dev/blog/com/blog/Admins/AdminService.java
@@ -0,0 +1,9 @@
+package dev.blog.com.blog.Admins;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AdminService {
+
+
+}
